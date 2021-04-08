f13971e366581d88f8029ae6aed3ab64fcf6eeb9
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-04-08 23:35:06 +0300

    todo save image

--- Awowed.JewelryStore/JewelryStore.Desktop/Views/ArticleWindow.xaml.cs
+++ Awowed.JewelryStore/JewelryStore.Desktop/Views/ArticleWindow.xaml.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Drawing;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using BarcodeLib;
+using JewelryStore.Desktop.Models;
 using JewelryStore.Desktop.ViewModels;
+using Microsoft.Win32;
+using Color = System.Drawing.Color;
+using Size = System.Windows.Size;
 
 namespace JewelryStore.Desktop.Views
 {
@@ -29,11 +29,67 @@ namespace JewelryStore.Desktop.Views
             _vm = vm;
         }
 
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using var memory = new MemoryStream();
+            bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            memory.Position = 0;
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memory;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+
         private void ArticleWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            var text = txtText.Text;
+            var text = _vm.BarCode;
             var barcode = new Barcode();
-            var barcodeImage = barcode.Encode(TYPE.EAN8, text, Color.Black, Color.White, pictureBox1.Width - 20, pictureBox1.Height - 20);
+            var barcodeImage = barcode.Encode(TYPE.EAN8, text, Color.Black, 
+                Color.White, (int)BarcodeImage.Width, (int)BarcodeImage.Height);
+            BarcodeImage.Source = BitmapToImageSource(new Bitmap(barcodeImage));
+
+            TbArticle.Text = _vm.ProdItem;
+            TbBarcode.Text = _vm.BarCode;
+            TbWeight.Text = $"{_vm.Weight}";
+            TbDate.Text = _vm.ArrivalDateString;
+            TbSize.Text = $"{_vm.ProdSize}";
+            TbPrice.Text = $"{_vm.Price}";
+            using (var context = new AppDbContext())
+            {
+                TbMetal.Text = $"{context.Metals.First(x => x.Id == _vm.IdMet).MetalName}";
+                TbSample.Text = $"{context.Metals.First(x => x.Id == _vm.IdMet).Sample}";
+                TbGroup.Text = $"{context.Prodgroups.First(x => x.Id == _vm.IdProdGr).ProdGroupName}";
+            }
+            ResultTbArticle.Text = $"Арт: {TbArticle.Text}";
+            ResultTbArticle2.Text = $"Арт: {TbArticle.Text}";
+            ResultTbGroup.Text = TbGroup.Text;
+            ResultTbMetalSample.Text = $"{TbMetal.Text} {TbSample.Text}°";
+            ResultTbPrice.Text = $"Ціна: {TbPrice.Text} UAH";
+            ResultTbPrice2.Text = $"Ціна: {TbPrice.Text} UAH";
+            ResultTbSize.Text = $"Розмір: {TbSize.Text}";
+            ResultTbWeight.Text = $"Вага: {TbWeight.Text}";
+            ResultTbWeight2.Text = $"Вага: {TbWeight.Text}";
+            ResultTbBarcode.Text = TbBarcode.Text;
+            ResultTbDate.Text = TbDate.Text;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new SaveFileDialog
+            {
+                Filter = "PNG files|*.png|All Files|*.*", 
+                Title = "Save diagram as PNG"
+            };
+            if (fileDialog.ShowDialog() != true) return;
+            var bitmap = new RenderTargetBitmap((int)PrintGrid.ActualWidth, (int)PrintGrid.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            bitmap.Render(PrintGrid);
+            using var stream = File.Create(fileDialog.FileName);
+            var encoder = new JpegBitmapEncoder { QualityLevel = 300 };
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+            encoder.Save(stream);
         }
     }
 }
